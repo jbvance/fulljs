91b0bb816ff7618e44b84d03ed639320cf683c06
Jason Vance <jbvance@gmail.com>
2017-04-03 06:49:45 -0500

    Add App and Header components

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './Header';
+
+const App = (props) => {
+  return (
+    <div className="App">
+      <Header message="Naming Contests" />
+      <div>Holding...</div>
+    </div>
+  );
+};
+
+export default App;
