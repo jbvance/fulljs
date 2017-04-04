4e91394ffc61259f6a6a65de6925dc2ee04466c4
Jason Vance <jbvance@gmail.com>
2017-04-04 11:56:13 -0500

    Add contest lookup for routing

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header';
 import axios from 'axios';
 import ContestList from './ContestList';
+import Contest from './Contest';
 
 const pushState = (obj, url) =>
   window.history.pushState(obj, '', url);
@@ -26,15 +27,32 @@ class App extends React.Component {
       `/contest/${contestId}`
     );
     //lookup the contest
+    //this.state.contests[contestId]
+    this.setState({
+      pageHeader: this.state.contests[contestId].contestName,
+      currentContestId: contestId
+    });
+  }
+
+  currentContent() {
+    if(this.state.currentContestId){
+      return (
+        <Contest {...this.state.contests[this.state.currentContestId]}/>
+      );
+    } else {
+      return (
+        <ContestList
+        onContestClick={this.fetchContest}
+        contests={this.state.contests} />
+    );
+    }
   }
 
   render () {
     return (
       <div className="App">
         <Header message={ this.state.pageHeader} />
-        <ContestList
-          onContestClick={this.fetchContest}
-          contests={this.state.contests} />
+        {this.currentContent()}
       </div>
     );
   }
